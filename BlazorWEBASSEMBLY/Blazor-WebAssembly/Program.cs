ff419f09b2b5ae815b023a7291dd38b05913ed66
M.Gabała <82496006+MGabala@users.noreply.github.com>
2022-11-13 13:08:50 +0100

    Auth0 - problem with authentication - not fixed..

--- BlazorWEBASSEMBLY/Blazor-WebAssembly/Program.cs
+++ BlazorWEBASSEMBLY/Blazor-WebAssembly/Program.cs
@@ -9,5 +9,10 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddHttpClient<IItemService,ItemService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
-
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.AdditionalProviderParameters.Add("audience", builder.Configuration["Auth0:Audience"]);
+});
 await builder.Build().RunAsync();
